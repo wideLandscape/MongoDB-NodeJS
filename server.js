ae60804f96da2d99ac77ec2b2f85f3b330981b34
Adriano Costa <iano76@gmail.com>
2019-07-19 11:27:29 +0200

    server layout

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const mongoose = require('mongoose'),
   config = require('./config.json');
 
+const Universities = require('./schemas/university.model');
+const Courses = require('./schemas/course.model');
+
 mongoose.Promise = global.Promise;
 mongoose
   .connect(config.DB, {
@@ -11,8 +14,36 @@ mongoose
   .then(
     () => {
       console.log('Database is connected');
+      groupQuery();
+      sumQuery();
+      filterQuery();
+      averageQuery();
+      geoSpatialQuery();
     },
     err => {
       console.log('Can not connect to the database' + err);
     }
   );
+/*
+    1. Number of courses categorized by “level” i.e. {level :”X”, “number” : Y}
+*/
+function groupQuery() {}
+/*
+    2. Number of students per university in the period 2014 - 2015 {“nameUniversity” : “number”}
+*/
+function sumQuery() {}
+/*
+    3. Courses with level “excellent” with 
+       university location, country and city {nameCourse : “x”, country:”y”,etc… }
+*/
+function filterQuery() {}
+/*
+    4. Average students per year for all universities
+*/
+function averageQuery() {}
+/*
+    5. Universities with the following constraints: 
+    •  -5.65 < x < -5.69, 
+    • -15 < y <=17,
+*/
+function geoSpatialQuery() {}

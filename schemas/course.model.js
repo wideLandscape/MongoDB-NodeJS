f6dc12f351e242c9d805ec8a4448e22f9a8888cd
Adriano Costa <iano76@gmail.com>
2019-07-19 12:52:46 +0200

    add virtual "info" to reference university

--- schemas/course.model.js
+++ schemas/course.model.js
@@ -18,5 +18,12 @@ const Course = new Schema(
     collection: 'courses'
   }
 );
-
+Course.virtual('info', {
+  ref: 'University', // The model to use
+  localField: 'university', // Find people where `localField`
+  foreignField: 'name', // is equal to `foreignField`
+  // If `justOne` is true, 'info' will be a single doc as opposed to
+  // an array. `justOne` is false by default.
+  justOne: false
+});
 module.exports = mongoose.model('Course', Course);
